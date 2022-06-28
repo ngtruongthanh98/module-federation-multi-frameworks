6da8ce3adc5dd9993c50f2202a1cd91b93aeec87
ThanhNT26 <ngtruongthanh98@gmail.com>
2022-06-28 22:44:30 +0700

    Change environment

--- body/webpack.config.js
+++ body/webpack.config.js
@@ -4,96 +4,95 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 
 module.exports = (_, argv) => ({
-    output: {
-        publicPath:
-            argv.mode === "development"
-                ? "http://localhost:3001/"
-                : "https://luca-webpack-mfe-body.surge.sh/",
-    },
+  output: {
+    publicPath:
+      argv.mode === "development"
+        ? "http://localhost:3001/"
+        : "https://luca-webpack-mfe-body.surge.sh/",
+  },
 
-    resolve: {
-        extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-    },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
 
-    devServer: {
-        port: 3001,
-        historyApiFallback: true,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods":
-                "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers":
-                "X-Requested-With, content-type, Authorization",
+  devServer: {
+    port: 3001,
+    historyApiFallback: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
         },
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.m?js/,
-                type: "javascript/auto",
-                resolve: {
-                    fullySpecified: false,
-                },
-            },
-            {
-                test: /\.(css|s[ac]ss)$/i,
-                use: ["style-loader", "css-loader", "postcss-loader"],
-            },
-            {
-                test: /\.(ts|tsx|js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                },
-            },
-        ],
-    },
-
-    plugins: [
-        new ModuleFederationPlugin({
-            name: "body",
-            filename: "remoteEntry.js",
-            // remotes: {
-            //     footer:
-            //         argv.mode === "development"
-            //             ? "footers@http://localhost:3002/remoteEntry.js"
-            //             : "footers@https://luca-webpack-mfe-footer.surge.sh/remoteEntry.js",
-            //     header:
-            //         argv.mode === "development"
-            //             ? "headers@http://localhost:8080/remoteEntry.js"
-            //             : "headers@https://luca-webpack-mfe-header.surge.sh/remoteEntry.js",
-            //     products:
-            //         argv.mode === "development"
-            //             ? "Products@http://localhost:3003/remoteEntry.js"
-            //             : "Products@https://luca-webpack-mfe-products.surge.sh/remoteEntry.js",
-            //     common:
-            //         argv.mode === "development"
-            //             ? "Common@http://localhost:3004/remoteEntry.js"
-            //             : "Common@https://luca-webpack-mfe-common.surge.sh/remoteEntry.js",
-            // },
-            remotes: {
-                footer: "footers@https://luca-webpack-mfe-footer.surge.sh/remoteEntry.js",
-                // header: "headers@https://luca-webpack-mfe-header.surge.sh/remoteEntry.js",
-                products:
-                    "Products@https://luca-webpack-mfe-products.surge.sh/remoteEntry.js",
-                common: "Common@https://luca-webpack-mfe-common.surge.sh/remoteEntry.js",
-            },
-            exposes: {},
-            shared: {
-                ...deps,
-                react: {
-                    singleton: true,
-                    requiredVersion: deps.react,
-                },
-                "react-dom": {
-                    singleton: true,
-                    requiredVersion: deps["react-dom"],
-                },
-            },
-        }),
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-        }),
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
     ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "body",
+      filename: "remoteEntry.js",
+      remotes: {
+        footer:
+          argv.mode === "development"
+            ? "footers@http://localhost:3002/remoteEntry.js"
+            : "footers@https://luca-webpack-mfe-footer.surge.sh/remoteEntry.js",
+        // header:
+        //     argv.mode === "development"
+        //         ? "headers@http://localhost:8080/remoteEntry.js"
+        //         : "headers@https://luca-webpack-mfe-header.surge.sh/remoteEntry.js",
+        products:
+          argv.mode === "development"
+            ? "Products@http://localhost:3003/remoteEntry.js"
+            : "Products@https://luca-webpack-mfe-products.surge.sh/remoteEntry.js",
+        common:
+          argv.mode === "development"
+            ? "Common@http://localhost:3004/remoteEntry.js"
+            : "Common@https://luca-webpack-mfe-common.surge.sh/remoteEntry.js",
+      },
+      // remotes: {
+      //     footer: "footers@https://luca-webpack-mfe-footer.surge.sh/remoteEntry.js",
+      //     // header: "headers@https://luca-webpack-mfe-header.surge.sh/remoteEntry.js",
+      //     products:
+      //         "Products@https://luca-webpack-mfe-products.surge.sh/remoteEntry.js",
+      //     common: "Common@https://luca-webpack-mfe-common.surge.sh/remoteEntry.js",
+      // },
+      exposes: {},
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 });
